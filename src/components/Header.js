f662c0c07856a5f16c63ffd8524ff7ce574adbd8
heyfranksmile@gmail.com <heyfranksmile@gmail.com>
2022-01-31 22:14:27 -0500

    adding header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,12 @@
 function Header() {
-  return <div>Task Tracker</div>;
+  return (
+    <>
+      <div className="header">
+        <h1>Task Tracker</h1>
+        <button className="btn">Add</button>
+      </div>
+    </>
+  );
 }
 
 export default Header;
