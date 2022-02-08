a9fcb422315e3e2dc6a5a1270861db8dd044ef77
heyfranksmile@gmail.com <heyfranksmile@gmail.com>
2022-02-07 19:54:48 -0500

    feat: adding tasks component

--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -1,9 +1,12 @@
 import React from "react";
+import Task from "./Task";
 
-function Tasks({ data }) {
-  console.log(data);
-
-  return <div>Hello</div>;
+function Tasks({ tasks, onDelete, onToggle }) {
+  return tasks.map((task) => {
+    return (
+      <Task key={task.id} task={task} onDelete={onDelete} onToggle={onToggle} />
+    );
+  });
 }
 
 export default Tasks;
