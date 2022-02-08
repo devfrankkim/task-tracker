92606beed9573ba187906654b39f364d7830c5cb
heyfranksmile@gmail.com <heyfranksmile@gmail.com>
2022-02-07 19:55:03 -0500

    added: dummy data

--- src/components/data.js
+++ src/components/data.js
@@ -1,7 +1,22 @@
 const DATA = [
-  { id: 1, text: "Doctors Appointment" },
-  { id: 2, text: "Meeting at School" },
-  { id: 3, text: "Food Shopping" },
+  {
+    id: 1,
+    text: "Doctors Appointment",
+    day: "2/15 at 3PM",
+    reminder: false,
+  },
+  {
+    id: 2,
+    text: "Meeting at School",
+    day: "2/16 at 3PM",
+    reminder: false,
+  },
+  {
+    id: 3,
+    text: "Food Shopping",
+    day: "2/17 at 3PM",
+    reminder: false,
+  },
 ];
 
 export default DATA;
